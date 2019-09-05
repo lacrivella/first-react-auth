b885e87336f83b72f0c6941eaa7102c7cc654d05
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-05 15:57:19 -0700

    created provider and store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import Auth0Provider from './Auth0Provider';
+import store from './store';
+import { Provider } from 'react-redux';
 
 render(
   <Auth0Provider
     domain="lacrivella.auth0.com"
     client_id="uB7qCZnvxreSEKisY6xRZfycF0Mb2qUB"
     redirect_uri="http://localhost:7890" >
-    <App />
+
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Auth0Provider>,
   document.getElementById('root')
 );
